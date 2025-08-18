40c1e7f43c48e6d2b8df53624a5c50b072040c22
JonasDevDE <jonasschnell23@gmail.com>
2025-08-18 02:31:32 +0200

    chore: initial commit

---
+++ backend/master/src/main/java/com/interguess/devbox/backend/master/MasterBootstrap.java
@@ -0,0 +1,8 @@
+package com.interguess.devbox.backend.master;
+
+public class MasterBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ backend/worker/src/main/java/com/interguess/devbox/backend/worker/WorkerBootstrap.java
@@ -0,0 +1,8 @@
+package com.interguess.devbox.backend.worker;
+
+public class WorkerBootstrap {
+
+    public static void main(String[] args) {
+        
+    }
+}
